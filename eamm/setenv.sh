ae01f579ef196f62cd2e1e2586c151ce5053247e
Ariel Antonitis <aantonitis@interprosoft.com>
2023-03-02 10:33:19 -0500

    Updated server.xml & setenv.sh

--- eamm/setenv.sh
+++ eamm/setenv.sh
@@ -3,14 +3,12 @@
 #export ENVIRONMENT_APPID=manage
 export ENVIRONMENT_APPID=ezmaxmobile
 export APP_URL=https://manage.inst8809.apps.sno8809.ezmaxcloud.com
-export AUTH_ISSUER_URL=https://auth.inst8809.apps.sno8809.ezmaxcloud.com/oidc/endpoint/MaximoAppSuite
-export AUTH_AUTHORIZATION_URL=https://auth.inst8809.apps.sno8809.ezmaxcloud.com/oidc/endpoint/MaximoAppSuite/authorize
-export AUTH_TOKEN_URL=https://auth.inst8809.apps.sno8809.ezmaxcloud.com/oidc/endpoint/MaximoAppSuite/token
-export VALIDATION_ENDPOINT_URL=https://auth.inst8809.apps.sno8809.ezmaxcloud.com/oidc/endpoint/MaximoAppSuite/introspect
-export AUTH_LOGOUT_URL=https://auth.inst8809.apps.sno8809.ezmaxcloud.com/oidc/endpoint/MaximoAppSuite/logout
-export JWK_ENDPOINT_URL=https://auth.inst8809.apps.sno8809.ezmaxcloud.com/oidc/endpoint/MaximoAppSuite/jwk
 export MAS_LOGOUT_URL=https://masdev.home.inst8809.apps.sno8809.ezmaxcloud.com/logout
-export DOMAIN_NAME=inst8809.apps.sno8809.ezmaxclou------pod.com
+# The OIDC URLs *must* be public (not internal service URLs), as users will be directed to them
+#export OIDC_BASE_URL=https://coreidp.mas-inst8809-core.svc/oidc/endpoint/MaximoAppSuite # doesn't work
+export OIDC_BASE_URL=https://auth.inst8809.apps.sno8809.ezmaxcloud.com/oidc/endpoint/MaximoAppSuite
+export OIDC_DISCOVERY_URL=${OIDC_BASE_URL}/.well-known/openid-configuration
+export DOMAIN_NAME=inst8809.apps.sno8809.ezmaxcloud.com
 export DB_SSL_ENABLED=nossl
 export MXE_DB_URL="jdbc:sqlserver://;serverName=54.209.23.69;databaseName=mas8snodemo;portNumber=1433;encrypt=true;trustServerCertificate=true;"
 export MXE_DB_SCHEMAOWNER=dbo
@@ -20,14 +18,14 @@ export MXE_MASDEPLOYED=1
 export MXE_USEAPPSERVERSECURITY=1
 export MXE_OLDAUTHINTICATIONTYPE=local
 export MXE_DB_USER=sa
-export MXE_DB_PASSWORD=Captain1
+export MXE_DB_PASSWORD=Captain1x
 export MXE_MASINTERNALAPI=https://internalapi.mas-inst8809-core.svc
 export MXE_MASINTERNALPUSHNOTIFAPI=https://push-notification-service-internal.mas-inst8809-core.svc
 export MXE_USESQLSERVERSEQUENCE=1
 #export CLIENT_ID=manage
 #export CLIENT_SECRET=rYNS1gPCvKPzjiETVFSkfQQ3ZbTaji1F
-export CLIENT_ID=ezmaxmobile
-export CLIENT_SECRET=4FvfzaMiNRRWEhZwUzPUEpaUJQ1li6C4ITszvk9Sqq7UtQbXte0hTLz7nUi8
+export CLIENT_ID=myemm2
+export CLIENT_SECRET=8zO0gOSGkmoRZDMR0QmrNSVodAkbWIE6m7MkK3kEIHGFVEAwgG7t8OrhYXWq
 export java_truststore=/opt/ibm/wlp/usr/servers/defaultServer/truststore/trust.p12
 export java_keystore=/opt/ibm/wlp/usr/servers/defaultServer/managefiles/key.p12
 export java_truststore_password=9AUZyNWe4jrUN0Xd
